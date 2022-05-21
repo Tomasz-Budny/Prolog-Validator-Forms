742d1a88bd2b1b069afd36f150e33c9f880953e2
Arizfurinmoto <sz.godon@gmail.com>
2022-05-21 21:56:48 +0200

    zmiana parametrow istniejacych elementow

--- PrologValidatorForms/Main.Designer.cs
+++ PrologValidatorForms/Main.Designer.cs
@@ -29,17 +29,22 @@ namespace PrologValidatorForms
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.btn_confirm = new System.Windows.Forms.Button();
             this.labelInfo = new System.Windows.Forms.Label();
+            this.btn_export = new System.Windows.Forms.Button();
             this.cb2 = new PrologValidatorForms.Eksplorator();
             this.cb1 = new PrologValidatorForms.Eksplorator();
-            this.btn_export = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_confirm
             // 
-            this.btn_confirm.Location = new System.Drawing.Point(21, 414);
+            this.btn_confirm.Location = new System.Drawing.Point(238, 463);
             this.btn_confirm.Name = "btn_confirm";
             this.btn_confirm.Size = new System.Drawing.Size(1023, 31);
             this.btn_confirm.TabIndex = 8;
@@ -52,27 +57,8 @@ namespace PrologValidatorForms
             this.labelInfo.AutoSize = true;
             this.labelInfo.Location = new System.Drawing.Point(719, 560);
             this.labelInfo.Name = "labelInfo";
-            this.labelInfo.Size = new System.Drawing.Size(133, 13);
+            this.labelInfo.Size = new System.Drawing.Size(0, 13);
             this.labelInfo.TabIndex = 9;
-            this.labelInfo.Text = "Jakiś tekst żeby nie zgubić";
-            // 
-            // cb2
-            // 
-            this.cb2.Location = new System.Drawing.Point(531, 40);
-            this.cb2.Margin = new System.Windows.Forms.Padding(1);
-            this.cb2.Name = "cb2";
-            this.cb2.Size = new System.Drawing.Size(525, 369);
-            this.cb2.TabIndex = 7;
-            this.cb2.Load += new System.EventHandler(this.cb2_Load);
-            // 
-            // cb1
-            // 
-            this.cb1.Location = new System.Drawing.Point(11, 40);
-            this.cb1.Margin = new System.Windows.Forms.Padding(1);
-            this.cb1.Name = "cb1";
-            this.cb1.Size = new System.Drawing.Size(525, 369);
-            this.cb1.TabIndex = 6;
-            this.cb1.Load += new System.EventHandler(this.cb1_Load);
             // 
             // btn_export
             // 
@@ -84,19 +70,82 @@ namespace PrologValidatorForms
             this.btn_export.UseVisualStyleBackColor = true;
             this.btn_export.Click += new System.EventHandler(this.btn_export_Click);
             // 
+            // cb2
+            // 
+            this.cb2.BackColor = System.Drawing.Color.Transparent;
+            this.cb2.Location = new System.Drawing.Point(819, 90);
+            this.cb2.Margin = new System.Windows.Forms.Padding(1);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(655, 369);
+            this.cb2.TabIndex = 7;
+            this.cb2.Load += new System.EventHandler(this.cb2_Load);
+            // 
+            // cb1
+            // 
+            this.cb1.BackColor = System.Drawing.Color.Transparent;
+            this.cb1.Location = new System.Drawing.Point(0, 90);
+            this.cb1.Margin = new System.Windows.Forms.Padding(1);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(655, 369);
+            this.cb1.TabIndex = 6;
+            this.cb1.Load += new System.EventHandler(this.cb1_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Załaduj projekt do analizy";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(834, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 29);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Załaduj plik z testami";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(590, 350);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 153);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 608);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1484, 601);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_export);
             this.Controls.Add(this.labelInfo);
             this.Controls.Add(this.btn_confirm);
             this.Controls.Add(this.cb2);
             this.Controls.Add(this.cb1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(1500, 640);
+            this.MinimumSize = new System.Drawing.Size(1500, 640);
             this.Name = "Main";
             this.Text = "Prolog Validator";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,6 +160,9 @@ namespace PrologValidatorForms
         private System.Windows.Forms.Button btn_confirm;
         private System.Windows.Forms.Label labelInfo;
         private System.Windows.Forms.Button btn_export;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
