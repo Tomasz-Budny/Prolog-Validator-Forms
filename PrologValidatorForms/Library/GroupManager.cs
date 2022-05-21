07230686b724c2acc5a6c2700fbeb188e2397b2b
Przemekpia <przemekpia@op.pl>
2022-05-21 23:42:29 +0200

    Dokonano zmian w metodzie CreateExcelFile() w klasie GroupManager

--- PrologValidatorForms/Library/GroupManager.cs
+++ PrologValidatorForms/Library/GroupManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using OfficeOpenXml;
 
 namespace PrologValidatorForms.Library
 {
@@ -41,6 +42,7 @@ namespace PrologValidatorForms.Library
                         this.vss.Add(vs);
                     }
                 }
+                CreateExcelFile();
             }
             else
             {
@@ -51,7 +53,43 @@ namespace PrologValidatorForms.Library
 
         public void CreateExcelFile()
         {
-            // Tu przem będziesz pisał metodę
+            using (ExcelPackage excelPackage = new ExcelPackage())
+            {
+                excelPackage.Workbook.Properties.Title = "Podsumowanie";
+                excelPackage.Workbook.Properties.Created = DateTime.Now;
+
+                ExcelWorksheet ws = excelPackage.Workbook.Worksheets.Add("Podsumowanie");
+
+                foreach (ValSolution item in vss)
+                {
+                    ws = excelPackage.Workbook.Worksheets.Add(item.SolutionName.Substring(3, 6));
+                    int dataCellsRow = 2;
+                    int dataCellsColumn = 2;
+                    ws.Cells[dataCellsRow, dataCellsColumn].Value = "Numer Albumu:";
+                    ws.Cells[dataCellsRow, dataCellsColumn + 1].Value = Convert.ToInt32(item.SolutionName.Substring(3, 6));
+                    ws.Cells[dataCellsRow + 2, dataCellsColumn].Value = "Numer podejścia:";
+                    ws.Cells[dataCellsRow + 2, dataCellsColumn + 1].Value = Convert.ToInt32(item.SolutionName.Substring(1, 1));
+                    ws.Cells[dataCellsRow + 1, dataCellsColumn].Value = "Grupa:";
+                    ws.Cells[dataCellsRow + 1, dataCellsColumn + 1].Value = Convert.ToInt32(item.SolutionName.Substring(10, 1));
+                }
+
+
+
+                try
+                {
+                    FileInfo fi = new FileInfo(destDir + @"\File.xlsx");
+                    excelPackage.SaveAs(fi);
+                    System.Diagnostics.Process.Start(destDir + @"\File.xlsx");
+                }
+                catch (System.InvalidOperationException ioe)
+                {
+                    label.Text += $"plik {destDir + @"\File.xlsx"} jest obecnie otwarty!, {ioe.Message}\n";
+                }
+                catch (Exception e)
+                {
+                    label.Text += $"Wystąpił niespodziewany błąd: {e.Message}\n";
+                }
+            }
         }
     }
 }
