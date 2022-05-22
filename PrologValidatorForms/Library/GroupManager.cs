e9c21df60391b13ff60297d599a75ee2bc3b2298
Przemekpia <przemekpia@op.pl>
2022-05-22 11:02:25 +0200

    Dodanie Informacji podstawowych i informacji o plikach do excela

--- PrologValidatorForms/Library/GroupManager.cs
+++ PrologValidatorForms/Library/GroupManager.cs
@@ -62,15 +62,48 @@ namespace PrologValidatorForms.Library
 
                 foreach (ValSolution item in vss)
                 {
-                    ws = excelPackage.Workbook.Worksheets.Add(item.SolutionName.Substring(3, 6));
-                    int dataCellsRow = 2;
-                    int dataCellsColumn = 2;
-                    ws.Cells[dataCellsRow, dataCellsColumn].Value = "Numer Albumu:";
-                    ws.Cells[dataCellsRow, dataCellsColumn + 1].Value = Convert.ToInt32(item.SolutionName.Substring(3, 6));
-                    ws.Cells[dataCellsRow + 2, dataCellsColumn].Value = "Numer podejścia:";
-                    ws.Cells[dataCellsRow + 2, dataCellsColumn + 1].Value = Convert.ToInt32(item.SolutionName.Substring(1, 1));
-                    ws.Cells[dataCellsRow + 1, dataCellsColumn].Value = "Grupa:";
-                    ws.Cells[dataCellsRow + 1, dataCellsColumn + 1].Value = Convert.ToInt32(item.SolutionName.Substring(10, 1));
+                    //Informacje podstawowe
+                    ws = excelPackage.Workbook.Worksheets.Add(item.SolutionName);
+                    int basicCellsRow = 2;
+                    int basicCellsColumn = 2;
+                    ws.Cells[basicCellsRow, basicCellsColumn, basicCellsRow, basicCellsColumn + 1].Merge = true;
+                    ws.Cells[basicCellsRow, basicCellsColumn].Value = "Informacje Podstawowe";
+
+                    ws.Cells[basicCellsRow + 1, basicCellsColumn].Value = "Numer Albumu:";
+                    ws.Cells[basicCellsRow + 1, basicCellsColumn + 1].Value = Convert.ToInt32(item.SolutionName.Substring(3, 6));
+                    ws.Cells[basicCellsRow + 2, basicCellsColumn].Value = "Numer podejścia:";
+                    ws.Cells[basicCellsRow + 2, basicCellsColumn + 1].Value = Convert.ToInt32(item.SolutionName.Substring(1, 1));
+                    ws.Cells[basicCellsRow + 3, basicCellsColumn].Value = "Grupa:";
+                    ws.Cells[basicCellsRow + 3, basicCellsColumn + 1].Value = Convert.ToInt32(item.SolutionName.Substring(10, 1));
+
+
+                    //Informacje o plikach
+
+                    int infCellsRow = 2;
+                    int infCellsColumn = 6;
+
+                    ws.Cells[infCellsRow, infCellsColumn, infCellsRow, infCellsColumn + item.Tasks.Count].Merge = true;
+                    ws.Cells[infCellsRow, infCellsColumn].Value = "Informacje o plikach";
+
+                    ws.Cells[infCellsRow + 1, infCellsColumn].Value = "Nazwa pliku";
+                    ws.Cells[infCellsRow + 2, infCellsColumn].Value = "Data utworzenia";
+                    ws.Cells[infCellsRow + 3, infCellsColumn].Value = "Rozmiar";
+
+                    infCellsColumn++;
+
+                    ws.Cells[10, 10].Value = item.Tasks.Count;
+
+                    for (int i = 0; i < item.Tasks.Count; i++)
+                    {
+                        ws.Cells[infCellsRow + 1, infCellsColumn + i].Value = item.Tasks[i].TaskName;
+                        ws.Cells[infCellsRow + 2, infCellsColumn + i].Value = item.Tasks[i].CreationTime;
+                        ws.Cells[infCellsRow + 3, infCellsColumn + i].Value = item.Tasks[i].SizeOfFile;
+                    }
+
+
+                    //Automatyczne wyrównanie kolumn
+
+                    ws.Cells[1, 1, 30, 30].AutoFitColumns();
                 }
 
 
