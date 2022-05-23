615dead223affd76bdeb4c63d583f4fb41cf2596
Przemysław <s205853@sggw.edu.pl>
2022-05-23 16:45:53 +0200

    Utworzoną klasę SizeConverter oraz metodę Convert

---
+++ PrologValidatorForms/Library/SizeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrologValidatorForms.Library
+{
+    static class SizeConverter
+    {
+        // Tu masz napisać to metodę Paweł
+        public static string Convert(long num)
+        {
+            return null;
+        }
+    }
+}
