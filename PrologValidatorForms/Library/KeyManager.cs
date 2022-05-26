2ffa1e641625394fe642c3da2ede69d045f79aeb
Gruby3773 <dwojtowicz1@o2.pl>
2022-05-26 18:05:16 +0200

    Stworzoną metodę AnalyzeKeyFile w klasie KeyManager

--- PrologValidatorForms/Library/KeyManager.cs
+++ PrologValidatorForms/Library/KeyManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,48 @@ namespace PrologValidatorForms.Library
 {
     class KeyManager
     {
-        List<DeclatedTask> DeclaratedTasks = new List<DeclatedTask>();
+        string keyFilePath;
+        List<DeclaredTask> declaredTasks = new List<DeclaredTask>();
 
-        class DeclatedTask
+        public KeyManager(string keyFilePath)
+        {
+            this.keyFilePath = keyFilePath;
+        }
+
+        public void AnalyzeKeyFile()
+        {
+            StreamReader sr = new StreamReader(keyFilePath);
+            string currentLine = "";
+
+            while ((currentLine = sr.ReadLine()) != null)
+            {
+                if(InputValidator.ValidateTaskName(currentLine))
+                {
+                    DeclaredTask dt = new DeclaredTask(currentLine);
+                    declaredTasks.Add(dt);
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void AddTestToLastTask(string currentLine)
         {
 
         }
 
-        class DeclaretedTest
+        class DeclaredTask
         {
+            string nameOfTask;
+            List<string> declaretedTests = new List<string>();
 
+            public DeclaredTask(string nameOfTask)
+            {
+                this.nameOfTask = nameOfTask;
+            }
         }
+
     }
 }
