c07ff80a452006aeade4602bfe5848412e79e640
Gruby3773 <dwojtowicz1@o2.pl>
2022-05-26 17:47:47 +0200

    Zadeklarowaną Klasę KeyManager

---
+++ PrologValidatorForms/Library/KeyManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrologValidatorForms.Library
+{
+    class KeyManager
+    {
+        List<DeclatedTask> DeclaratedTasks = new List<DeclatedTask>();
+
+        class DeclatedTask
+        {
+
+        }
+
+        class DeclaretedTest
+        {
+
+        }
+    }
+}
